04f38f366129d375d0d0d27c062c473e1a06dd12
Rob <robert@invoicemaker.com>
2024-08-18 21:30:48 +0100

    remove

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { CapacitorHealthKitPlugin } from './definitions';
-declare const CapacitorHealthKit: CapacitorHealthKitPlugin;
-export * from './definitions';
-export { CapacitorHealthKit };
