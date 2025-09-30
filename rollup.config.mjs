a3d42c51d170f06c949ae2d6e556466265f1f3ee
Rob <robert@invoicemaker.com>
2025-09-30 14:13:51 +0100

    Bump to new capacitor version

---
+++ rollup.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'rollup';
+
+export default defineConfig({
+  input: 'dist/esm/index.js',
+  output: [
+    {
+      file: 'dist/plugin.js',
+      format: 'iife',
+      name: 'CapacitorHealthkitWeight',
+      globals: { '@capacitor/core': 'capacitorExports' },
+      sourcemap: true,
+      inlineDynamicImports: true,
+    },
+    { file: 'dist/plugin.cjs.js', format: 'cjs', sourcemap: true, inlineDynamicImports: true },
+  ],
+  external: ['@capacitor/core'],
+});
